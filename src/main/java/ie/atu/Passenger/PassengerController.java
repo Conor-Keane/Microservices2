0f3b1e4c19ca9633d73396516d2cf6bf7729041c
Conor-Keane <115028044+Conor-Keane@users.noreply.github.com>
2022-11-16 13:34:27 +0000

    Revert "Added a method that can be identified by the passenger ID"

--- src/main/java/ie/atu/Passenger/PassengerController.java
+++ src/main/java/ie/atu/Passenger/PassengerController.java
@@ -1,32 +1,22 @@
 package ie.atu.Passenger;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-@SpringBootApplication
-@RestController
+
 @RequestMapping(path = "api/passenger")
 
 public class PassengerController {
     public List<Passenger> myPassengers = List.of(
-            new Passenger("Mr", "Conor", "12345678910", "0879543461", 21),
-            new Passenger("Mr", "Conor2", "4567891011", "091098765", 24),
-            new Passenger("Mr", "Conor3", "78910111213", "087123456", 21)
+            new Passenger("Mr", "Conor", "123", "0879543461", 21),
+            new Passenger("Mr", "Conor2", "456", "091098765", 24),
+            new Passenger("Mr", "Conor3", "789", "087123456", 21)
     );
-
 
     @GetMapping
     public List<Passenger> getMyPassengers() {
         return myPassengers;
-    }
-
-    @GetMapping("/{passengerID}")
-    public Passenger getPassenger(@PathVariable String passengerID) {
-        return new Passenger("Mr", "Conor", "1234", "0879543461", 21);
     }
 
 }
