d8804a405d8f3bcd1ab109fe00a7454f9de6ee82
conor-keane <g00391846@atu.ie>
2022-11-16 12:15:40 +0000

    Added Passenger Test Class and copied in details of passenger class from assignment

--- src/main/java/ie/atu/Passenger/Passenger.java
+++ src/main/java/ie/atu/Passenger/Passenger.java
@@ -1,4 +1,83 @@
 package ie.atu.Passenger;
 
+
 public class Passenger {
+    private String Title;
+    private String Name;
+    private String ID;
+    private String PhoneNo;
+    private int Age;
+
+    public void setTitle(String title) {
+
+        if(("Mr").equalsIgnoreCase(title) || ("Mrs").equalsIgnoreCase(title) || ("Ms").equalsIgnoreCase(title)) {
+            Title = title;
+        }
+        else {
+            throw new IllegalArgumentException("Please enter Mr, Mrs or Ms");
+        }
+
+    }
+
+    public void setName(String name) {
+        int count = name.length();
+        if (count >= 3) {
+            Name = name;
+        }
+        else {
+            throw new IllegalArgumentException("Name must be greater than 3 characters long");
+        }
+
+    }
+
+    public void setID(String id) {
+        int count = id.length();
+        if (count >= 10) {
+            ID = id;
+        }
+        else {
+            throw new IllegalArgumentException("ID must be greater than 10 characters long");
+        }
+
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        int count = phoneNo.length();
+        if (count > 6) {
+            PhoneNo = phoneNo;
+        }
+        else {
+            throw new IllegalArgumentException("Phone number must be greater than 6 characters long");
+        }
+
+    }
+
+    public void setAge(int age) {
+        if (age > 16) {
+            Age = age;
+        }
+        else {
+            throw new IllegalArgumentException("You must be over the age of 16 to fly");
+        }
+
+    }
+    public String getTitle() {
+        return Title;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public String getPhoneNo() {
+        return PhoneNo;
+    }
+
+    public int getAge() {
+        return Age;
+    }
 }
