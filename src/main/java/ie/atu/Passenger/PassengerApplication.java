5c4512245fc3652d12cf16a17724ba54ab7da944
conor-keane <g00391846@atu.ie>
2022-11-16 12:41:42 +0000

    Added method to PassengerControl class

--- src/main/java/ie/atu/Passenger/PassengerApplication.java
+++ src/main/java/ie/atu/Passenger/PassengerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class PassengerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/ie/atu/Passenger/PassengerController.java
+++ src/main/java/ie/atu/Passenger/PassengerController.java
@@ -1,4 +1,22 @@
 package ie.atu.Passenger;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequestMapping(path = "api/passenger")
+
 public class PassengerController {
+    public List<Passenger> myPassengers = List.of(
+            new Passenger("Mr", "Conor", "123", "0879543461", 21),
+            new Passenger("Mr", "Conor2", "456", "091098765", 24),
+            new Passenger("Mr", "Conor3", "789", "087123456", 21)
+    );
+
+    @GetMapping
+    public List<Passenger> getMyPassengers() {
+        return myPassengers;
+    }
+
 }
